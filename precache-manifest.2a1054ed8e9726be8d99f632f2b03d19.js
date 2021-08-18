f93fb0b74764c783481f1b30f3828dc2a5eb60c5
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-08-19 02:13:36 +0500

    Updates

---
+++ precache-manifest.2a1054ed8e9726be8d99f632f2b03d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bcad8d169632e7402a3",
+    "url": "/mobVibe/static/css/main.29b44030.chunk.css"
+  },
+  {
+    "revision": "3bcad8d169632e7402a3",
+    "url": "/mobVibe/static/js/main.3bcad8d1.chunk.js"
+  },
+  {
+    "revision": "fbf1e145251955889581",
+    "url": "/mobVibe/static/js/runtime~main.fbf1e145.js"
+  },
+  {
+    "revision": "f84fdc4cb746b88d70df",
+    "url": "/mobVibe/static/js/2.f84fdc4c.chunk.js"
+  },
+  {
+    "revision": "cf7c11da0afb721295842877e1918145",
+    "url": "/mobVibe/static/media/V.cf7c11da.png"
+  },
+  {
+    "revision": "06c0fd0d03aa211bc8079d207999f51d",
+    "url": "/mobVibe/index.html"
+  }
+];
